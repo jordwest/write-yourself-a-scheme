80f1e5f4eba46a248bf242149a1e861c3c06c72d
Jordan West <jordwest@gmail.com>
2017-04-29 17:26:30 +1000

    Symbol parsing

--- main.hs
+++ main.hs
@@ -1,7 +1,16 @@
 module Main where
 import System.Environment
+import Text.ParserCombinators.Parsec hiding (spaces)
 
 main :: IO ()
 main = do
-    args <- getArgs
-    putStrLn ("Hello, " ++ args !! 0)
+    (expr:_) <- getArgs
+    putStrLn (readExpr expr)
+
+symbol :: Parser Char
+symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
+
+readExpr :: String -> String
+readExpr input = case parse symbol "lisp" input of
+    Left  err -> "No match: " ++ show err
+    Right val -> "Found value"
