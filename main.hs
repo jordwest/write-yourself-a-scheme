7151d19109fc1cf6b1d350f1b5907e6a72f7b1c1
Jordan West <jordwest@gmail.com>
2017-04-29 18:20:15 +1000

    parses atoms, numbers, strings

--- main.hs
+++ main.hs
@@ -1,21 +1,58 @@
 module Main where
+
+import Control.Monad
 import System.Environment
 import Text.ParserCombinators.Parsec hiding (spaces)
+
+-- All possible lisp values
+data LispVal = Atom String
+             | List [LispVal]
+             | DottedList [LispVal] LispVal
+             | Number Integer
+             | String String
+             | Bool Bool
 
 main :: IO ()
 main = do
     (expr:_) <- getArgs
     putStrLn (readExpr expr)
 
+-- https://en.wikibooks.org/wiki/Write_Yourself_a_Scheme_in_48_Hours/Parsing
+
+readExpr :: String -> String
+readExpr input = case parse parseExpr "lisp" input of
+    Left  err -> "No match: \n" ++ show err
+    Right val -> "Found value"
+
+parser = spaces >> symbol
+
+parseExpr :: Parser LispVal
+parseExpr = parseAtom
+         <|> parseString
+         <|> parseNumber
+
+parseAtom :: Parser LispVal
+parseAtom = do
+    first <- letter <|> symbol
+    rest <- many (letter <|> digit <|> symbol)
+    let atom = first:rest
+    return $ case atom of
+        "#t" -> Bool True
+        "#f" -> Bool False
+        _    -> Atom atom
+
+parseString :: Parser LispVal
+parseString = do
+    char '"'
+    x <- many (noneOf "\"")
+    char '"'
+    return $ String x
+
+parseNumber :: Parser LispVal
+parseNumber = liftM (Number . read) (many1 digit)
+
 symbol :: Parser Char
 symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
 
 spaces :: Parser ()
 spaces = skipMany1 space
-
-parser = spaces >> symbol
-
-readExpr :: String -> String
-readExpr input = case parse parser "lisp" input of
-    Left  err -> "No match: \n" ++ show err
-    Right val -> "Found value"
